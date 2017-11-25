f72ba97cc3c147cd5ae0c9271ca01e1938d89825
Corneel <ceejay@Corneels-MacBook-Pro.local>
2017-11-25 12:48:41 +0100

    reformat

--- aldryn_people/urls.py
+++ aldryn_people/urls.py
@@ -12,21 +12,14 @@ from aldryn_people.views import (
 )
 
 urlpatterns = [
-    url(r'^group/(?P<pk>[0-9]+)/$',
-        GroupDetailView.as_view(), name='group-detail'),
-    url(r'^group/(?P<slug>[A-Za-z0-9_\-]+)/$',
-        GroupDetailView.as_view(), name='group-detail'),
+    url(r'^group/(?P<pk>[0-9]+)/$',GroupDetailView.as_view(), name='group-detail'),
+    url(r'^group/(?P<slug>[A-Za-z0-9_\-]+)/$', GroupDetailView.as_view(), name='group-detail'),
+    
+    url(r'^(?P<pk>[0-9]+)/$',PersonDetailView.as_view(), name='person-detail'),
+    url(r'^(?P<slug>[A-Za-z0-9_\-]+)/$',PersonDetailView.as_view(), name='person-detail'),
 
-    url(r'^(?P<pk>[0-9]+)/$',
-        PersonDetailView.as_view(), name='person-detail'),
-    url(r'^(?P<slug>[A-Za-z0-9_\-]+)/$',
-        PersonDetailView.as_view(), name='person-detail'),
+    url(r'^(?P<pk>[0-9]+)/download/$',DownloadVcardView.as_view(), name='download_vcard'),
+    url(r'^(?P<slug>[A-Za-z0-9_\-]+)/download/$',DownloadVcardView.as_view(), name='download_vcard'),
 
-    url(r'^(?P<pk>[0-9]+)/download/$',
-        DownloadVcardView.as_view(), name='download_vcard'),
-    url(r'^(?P<slug>[A-Za-z0-9_\-]+)/download/$',
-        DownloadVcardView.as_view(), name='download_vcard'),
-
-    url(r'^$',
-        GroupListView.as_view(), name='group-list'),
+    url(r'^$',GroupListView.as_view(), name='group-list'),
 ]
